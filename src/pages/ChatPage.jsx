44636068c4e164894ab29502c4f6bf620ea61d9e
antoinevalton <antoinevalton@proton.me>
2026-06-08 08:00:50 +0000

    Manual code change

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -651,6 +651,7 @@ export default function ChatPage() {
         customSlug={customSlug || convId}
         appSettings={appSettings}
         onUpdateSettings={handleUpdateAppMeta}
+        isPublic={true}
       />
       
       {/* Sidebar */}
